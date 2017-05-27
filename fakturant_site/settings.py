c464c5d48d665d2ca0dec482c985a0894027f034
Milan Peković <milanpekovic89@gmail.com>
2017-05-27 23:59:06 +0200

    Update settings.py

--- fakturant_site/settings.py
+++ fakturant_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j3ksc72c99ir59i&7jgrsky9!xweaco=4s1cmai1px*yl)v57k'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
